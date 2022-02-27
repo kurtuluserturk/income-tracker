71c551736cfd18643b1d7c7443fee42efe4e9172
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2022-02-27 23:45:40 +0300

    IncomeList created

---
+++ src/components/IncomeList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="income-list"></div>
+  <IncomeItem
+    v-for="data in state.sortedIncome"
+    :key="data.id"
+    :income="data"
+  />
+</template>
+
+<script>
+import IncomeItem from "./IncomeItem";
+
+export default {
+  name: "IncomeList",
+  components: {
+    IncomeItem,
+  },
+  props: {
+    state: Object,
+  },
+  setup() {
+    return {
+      IncomeItem,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.income-list {
+  margin-top: 30px;
+  padding: 15px;
+}
+</style>
