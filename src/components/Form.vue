b2b45a5fced63e021ca0b742b8e343c3d91e0754
Kurtuluş ERTÜRK <kurtulus@efabrika.com>
2022-02-27 20:04:39 +0300

    form created

---
+++ src/components/Form.vue
@@ -0,0 +1,94 @@
+<template>
+  <form @submit.prevent="formHandler">
+    <input
+      type="text"
+      placeholder="Description..."
+      v-model="formData.desc"
+      required
+    />
+    <input
+      type="number"
+      placeholder="Amount..."
+      v-model="formData.amount"
+      required
+    />
+    <input type="date" placeholder="Date..." v-model="formData.date" required />
+    <input type="submit" value="SUBMIT" />
+  </form>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  props: {
+    state: Object,
+  },
+  setup(props, { emit }) {
+    const formData = reactive({
+      desc: null,
+      amount: null,
+      date: null,
+    });
+
+    function formHandler() {
+      emit("add-income", {
+        desc: formData.desc,
+        amount: parseInt(formData.amount),
+        date: formData.date,
+      });
+
+      formData.desc = null;
+      formData.amount = null;
+      formData.date = null;
+    }
+
+    return {
+      formData,
+      formHandler,
+    };
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+}
+form input {
+  color: #888;
+  border: none;
+  outline: none;
+  font-size: 20px;
+}
+form input::placeholder {
+  color: #aaa;
+}
+form input:not([type="submit"]) {
+  display: block;
+  background: #fff;
+  border: none;
+  outline: none;
+  padding: 5px 15px;
+}
+form input[type="submit"] {
+  display: block;
+  background: none;
+  border: none;
+  outline: none;
+  color: #fff;
+  font-weight: 500;
+  text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.2);
+  padding: 5px 15px;
+  background-color: #ffce00;
+  cursor: pointer;
+}
+form input:first-of-type {
+  border-radius: 8px 0px 0px 8px;
+}
+form input:last-of-type {
+  border-radius: 0px 8px 8px 0px;
+}
+</style>
